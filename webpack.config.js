3ed604b7c6723cdb7213a699978446bc5f9c6c4b
NicolasMaxime <maxime1.nicolas@gmail.com>
2022-06-23 15:46:31 +0200

    watch mode

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,23 @@
 const { mode } = require("webpack-nano/argv");
+
+//generate index.html in dist
 const { MiniHtmlWebpackPlugin } = require("mini-html-webpack-plugin");
+
+//Hot reload module by instantiating a server
+const { WebpackPluginServe } = require("webpack-plugin-serve");
 
 module.exports = {
   mode,
-  plugins: [new MiniHtmlWebpackPlugin({ context: { title: "Demo" } })],
+  watch: mode === "development",
+  entry: ["./src", "webpack-plugin-serve/client"],
+  plugins: [
+    new MiniHtmlWebpackPlugin({ context: { title: "Demo" } }),
+    new WebpackPluginServe({
+      port: process.env.PORT || 8080,
+      static: "./dist",
+      liveReload: true,
+      waitForBuild: true,
+      historyFallback: true
+      }),
+  ],
 };
